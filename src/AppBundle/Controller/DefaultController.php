ff03f0022135bebf8a011eced196d56590d6354e
boltun_oreh <alazankin@gmail.com>
2017-05-31 00:15:36 +0300

    fix controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,17 +2,48 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Project;
+use AppBundle\Entity\Category;
+use AppBundle\Entity\Order;
+use AppBundle\Form\OrderType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Project;
 
 class DefaultController extends Controller
 {
     /**
+     * @Route("/about", name="app__about")
+     */
+    public function aboutAction()
+    {
+        $teammates = $this->getDoctrine()->getRepository('AppBundle:Teammate')->findBy(
+            array(
+                'isActive' => true,
+            ),
+            array(
+                'sortOrder' => 'ASC',
+            )
+        );
+        $clients = $this->getDoctrine()->getRepository('AppBundle:Client')->findBy(
+            array(
+                'isActive' => true,
+            ),
+            array(
+                'sortOrder' => 'ASC',
+            )
+        );
+
+        return $this->render('default/about.html.twig', array(
+            'teammates' => $teammates,
+            'clients'   => $clients,
+        ));
+    }
+
+    /**
      * @Route("/", name="app__homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         $projects = $this->getDoctrine()->getRepository('AppBundle:Project')->findBy(array(
             'isActive'   => true,
@@ -34,7 +65,17 @@ class DefaultController extends Controller
      */
     public function exclusiveAction()
     {
-        return $this->render('default/exclusive.html.twig', array(
+        $projects = $this->getDoctrine()->getRepository('AppBundle:Project')->findBy(array(
+            'isActive'   => true,
+        ));
+
+        $categories = $this->getDoctrine()->getRepository('AppBundle:Category')->findBy(array(
+            'isActive' => true,
+        ));
+
+        return $this->render('default/project_exclusive.html.twig', array(
+            'projects'   => $projects,
+            'categories' => $categories,
         ));
     }
 
@@ -43,26 +84,21 @@ class DefaultController extends Controller
      */
     public function projectAction(Project $project)
     {
+        $projects = $this->getDoctrine()->getRepository('AppBundle:Project')->findBy(array(
+            'isActive' => true,
+        ));
+
+        $categories = $this->getDoctrine()->getRepository('AppBundle:Category')->findBy(array(
+            'isActive' => true,
+        ));
+
+        $form = $this->createForm(OrderType::class);
+
         return $this->render('default/project.html.twig', array(
-            'project' => $project,
-        ));
-    }
-
-    /**
-     * @Route("/about", name="app__about")
-     */
-    public function aboutAction()
-    {
-        $teammates = $this->getDoctrine()->getRepository('AppBundle:Teammate')->findBy(array(
-            'isActive' => true,
-        ));
-        $clients = $this->getDoctrine()->getRepository('AppBundle:Client')->findBy(array(
-            'isActive' => true,
-        ));
-
-        return $this->render('default/about.html.twig', array(
-            'teammates' => $teammates,
-            'clients'   => $clients,
+            'project'    => $project,
+            'projects'   => $projects,
+            'categories' => $categories,
+            'order_form' => $form,
         ));
     }
 }
