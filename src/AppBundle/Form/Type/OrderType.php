26a1ed966c7781f3f0ae8f22061f388716129379
Pavel Alazankin <alazankin@gmail.com>
2017-06-13 10:57:40 +0300

    add copyMe to order form

--- src/AppBundle/Form/Type/OrderType.php
+++ src/AppBundle/Form/Type/OrderType.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace AppBundle\Form;
+namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
@@ -50,6 +51,10 @@ class OrderType extends AbstractType
             ))
             ->add('ideas', TextareaType::class, array(
                 'label' => false,
+            ))
+            ->add('copyMe', CheckboxType::class, array(
+                'label' => false,
+                'required' => false,
             ))
         ;
 
